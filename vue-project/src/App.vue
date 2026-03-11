64729ce575602cf387c3d3906fcac9ce89d72f61
flowersyie <nursafahsaskia@gmail.com>
2026-03-11 14:38:51 +0700

    Icons & App

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,85 +1,61 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/icons/Counter.vue'
+// // reactive state 
+// const count = ref(0)
+
+// // trigger reactivity
+// function increment() {
+//   count.value++
+// }
+
+// const user = reactive({
+//   name: 'Marklee',
+//   age: 25,
+//   email: 'marklee@example.com',
+// })
+
+// const tambahUmur = () => {
+//   user.age++
+// }
+
+// const kurangiUmur = () => {
+//   user.age--
+// }
+
+// // lifecycle hook
+// onMounted(() => {
+//   console.log('The initial count is:', count.value)
+// })
+
+// const todos = reactive([ 'Mandi', 'Makan', 'Tidur' ])
+
+// const addTodo = () => {
+//   todos.push('Mengaji')
+// }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <Counter />
+  <!-- <div>
+    <p> name: {{ user.name }}</p>
+    <p> age: {{ user.age }}</p>
+    <p> email: {{ user.email }}</p>
+    <button @click="kurangiUmur">Kurang</button>
+    <button @click="tambahUmur">Tambah</button>
+  </div> -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <!-- <div>
+    <h3>Kegiatan Sehari - hari</h3>
+    <ul>
+      <li v-for="todo in todos" :key="todo">
+        {{ todo }}
+      </li>
+    </ul>
+    <button @click="addTodo">Tambah Kegiatan</button>
+  </div> -->
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+ 
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scooped></style>
---
+++ vue-project/src/components/icons/Counter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// trigger reactivity
+function increment() {
+  count.value++
+} 
+</script>
+
+<template>
+  <div>
+    <button @click="increment">Click Me {{ count }}</button>
+  </div>
+</template>
